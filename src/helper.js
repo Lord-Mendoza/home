3b80a2775536cc254fd2d5d34525ac4f1a823f70
lord-mendoza <lord.d.mendoza@gmail.com>
2019-02-19 09:50:55 -0500

    Added Files

---
+++ src/helper.js
@@ -0,0 +1,6 @@
+function dabOnThemHaters(){
+	alert("clicked");
+	document.getElementById("profPic").src='https://media0.giphy.com/media/A4R8sdUG7G9TG/giphy.gif?cid=3640f6095c66ce1e4f4b4a716761d7c6';
+}
+
+document.getElementById("profPic").addEventListener("click", dabOnThemHaters);
